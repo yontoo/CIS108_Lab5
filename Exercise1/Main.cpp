8fbcf0d4e717f33bc4f06385f03065f785afb1c3
yontoo <traylee1513@yahoo.com>
2019-02-18 21:06:44 -0800

    Initial commit.

---
+++ Exercise1/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "Commands in this program." << std::endl;
+	std::cout << "1. add - Add a new song to the database." << std::endl;
+	std::cout << "2. list - List the songs in the database." << std::endl;
+}
