57ba955c5fba7a990f3aa1a410dd13214a5979b8
Tray Harris <traylee1513@yahoo.com>
2019-02-18 21:09:38 -0800

    Initial commit.

--- Exercise1/Main.cpp
+++ Exercise1/Main.cpp
@@ -6,4 +6,5 @@ int main()
 	std::cout << "Commands in this program." << std::endl;
 	std::cout << "1. add - Add a new song to the database." << std::endl;
 	std::cout << "2. list - List the songs in the database." << std::endl;
+	std::cout << "3. save - Save the songs to the database." << std::endl;
 }
